58eb98ab40d8221271131abd2c3edb9d7f5e30be
Giandomenico Olini <giando.olini@gogodigital.it>
2016-11-19 13:57:51 +0100

    Update wp-gogo-customization.php

--- wp-gogo-customization/wp-gogo-customization.php
+++ wp-gogo-customization/wp-gogo-customization.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/cinghie/wordpress-gogo-customization
  * Description: Customize your Wordpress site
  * Author: Gogodigital S.r.l.s.
- * Version: 1.0.5
+ * Version: 1.0.6
  * Author URI: http://www.gogodigital.it
  */
 
@@ -61,5 +61,24 @@ function wpdocs_custom_excerpt_length( $length ) {
 	}
 }
 add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+
+/**
+ * Get the first image in post_content.
+ */
+
+function catch_that_image() {
+    global $post, $posts;
+    $first_img = '';
+    ob_start();
+    ob_end_clean();
+    $output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+    $first_img = $matches [1] [0];
+
+    if(empty($first_img)){ //Defines a default image
+        $first_img = "/images/default.jpg";
+    }
+	
+    return $first_img;
+}
 
 ?>
