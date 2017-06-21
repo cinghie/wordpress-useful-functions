3946776d0b31abbd0669960db82e5f55ce683088
Giandomenico Olini <giando.olini@gogodigital.it>
2017-06-21 16:06:44 +0200

    Update wp-gogo-customization.php

--- wp-gogo-customization/wp-gogo-customization.php
+++ wp-gogo-customization/wp-gogo-customization.php
@@ -94,4 +94,25 @@ add_filter( 'http_request_args', function ( $response, $url ) {
 	return $response;
 }, 10, 2 );
 
+/**
+ * Redirect to custom url on failed login attempt
+ */
+function front_end_login_fail( $username ) {
+    $referrer = $_SERVER['HTTP_REFERER'];  // where did the post submission come from?
+    // if there's a valid referrer, and it's not the default log-in screen
+    if ( !empty($referrer) && !strstr($referrer,'wp-login') && !strstr($referrer,'wp-admin') ) {
+        $pos = strpos($referrer, '?login=failed');
+        if($pos === false) {
+            // add the failed
+            wp_redirect( $referrer . '?login=failed' );  // let's append some     information (login=failed) to the URL for the theme to use
+        }
+        else {
+            // already has the failed don't appened it again
+            wp_redirect( $referrer );  // already appeneded redirect back
+        }
+        exit;
+    }
+}
+add_action( 'wp_login_failed', 'front_end_login_fail' );
+
 ?>
